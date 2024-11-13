94ef591cdeba36e3ea3892211afed1259c3fbdb8
UrasaJoshua <urasajoshuag@gmail.com>
2024-11-13 12:15:42 +0300

    changes

--- aru/settings.py
+++ aru/settings.py
@@ -145,8 +145,8 @@ REST_FRAMEWORK = {
 
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',  
-    'https://database-mu-six.vercel.app',
+    'http://localhost:5173',  # Your local development server
+    'https://database-mu-six.vercel.app',  # Vercel frontend URL
 ]
 
 
